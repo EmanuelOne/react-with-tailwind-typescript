2ccc44d6f88dea02857050c73ff7af19c19d1309
emanuelone <emanuel4mechie@gmail.com>
2022-01-09 22:51:51 +0100

    redone

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import Login from './pages/login';
+import Register from './pages/register';
+export const routers = [
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    component: Register,
+  },
+];
